3832c75fbee5bec2318a46aff3e37df7611d0698
Dhruv Unmesh Patil <universo_esplorar@Dhruvs-MacBook-Air.local>
2024-01-18 17:58:10 +0530

    part-1

---
+++ src/components/QuestionBox.jsx
@@ -0,0 +1,12 @@
+import React,{ useState } from 'react';
+import "./Quizbox.css"
+
+export default function QuestionBox() {
+
+  return (
+    <>
+    <div className='main-conatiner'></div>
+    </>
+  )
+}
+
